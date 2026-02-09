3b5310ba098476b7b37e054be7efe85b59556648
VANSH-AGARWAL9264 <3pm2022vansh@gmail.com>
2026-02-09 17:13:00 +0000

    operations

--- 1_basics/03_conversionOperations.js
+++ 1_basics/03_conversionOperations.js
@@ -30,3 +30,26 @@
 
 
 // *********************************************************     Operations     *********************************************************
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+// let str1 = "Vansh"
+// let str2 = " Agarwal"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
