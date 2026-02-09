7f1bf72991d407517773d266d06b23b6c0d1c2ca
VANSH-AGARWAL9264 <3pm2022vansh@gmail.com>
2026-02-09 17:43:45 +0000

    datatypes_summary

---
+++ 1_basics/04_datatypes_summary.js
@@ -0,0 +1,27 @@
+// # 01. Primitive dataTypes:
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// // Symbol:
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId); // false(this is what Symbol mean to us)
+
+
+
+
+// # 02. Referennce Type (Non-Primitive):
+// 3 types : Array, Objects, Functions
+
+// // a. Arrays:
+const heros = ["shaktiman", "naagraj", "doga"]
+// //  b. Objects:
+let myObj = {
+    name: "Vansh",
+    age: 22,
+}
+
+// // c. functions:
+const myFunction = function() {
+    console.log("Hello Welcome Vansh");
+}
