b59a71f265109dd6a844873d80430d58eb880611
VANSH-AGARWAL9264 <3pm2022vansh@gmail.com>
2026-02-09 18:00:17 +0000

    memory

--- 1_basics/04_datatypes_summary.js
+++ 1_basics/04_datatypes_summary.js
@@ -13,15 +13,42 @@
 // # 02. Referennce Type (Non-Primitive):
 // 3 types : Array, Objects, Functions
 
-// // a. Arrays:
-const heros = ["shaktiman", "naagraj", "doga"]
-// //  b. Objects:
-let myObj = {
-    name: "Vansh",
-    age: 22,
+// // // a. Arrays:
+// const heros = ["shaktiman", "naagraj", "doga"]
+// // //  b. Objects:
+// let myObj = {
+//     name: "Vansh",
+//     age: 22,
+// }
+
+// // // c. functions:
+// const myFunction = function() {
+//     console.log("Hello Welcome Vansh");
+// }
+
+
+
+
+// *********************************************************     Stack & Heap Memory in JS     *********************************************************
+// Stack (Primitive){makes copy},                                                Heap(Non-Primitive){takes references of original & make changes in it}
+
+let myYoutubename = "hiteshchaudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
 
-// // c. functions:
-const myFunction = function() {
-    console.log("Hello Welcome Vansh");
-}
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
