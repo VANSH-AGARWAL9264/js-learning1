302622daa076135859c35651ad6ca74b429fb78d
VANSH-AGARWAL9264 <3pm2022vansh@gmail.com>
2026-02-11 06:33:29 +0000

    name chnage

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,10 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//  strict check === [yaha dataType change hote hi problem he.]
+console.log("2" === 2);
