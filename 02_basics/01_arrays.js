b7f35cd495e3d47663100fc54e2fc423b3d21751
VANSH-AGARWAL9264 <3pm2022vansh@gmail.com>
2026-06-10 05:25:41 +0000

    minor improvement

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -9,36 +9,36 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 
 // Array Methods 
-// console.log(myArr);
+console.log(myArr);
 
 // myArr.push(6)
 // myArr.push(7)
 // myArr.pop()
 // console.log(myArr);
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9) ////<--unshift adds that element (9) in the starting of array.
+// myArr.shift() ////<-- shift removes that element from the starting of array.
 // console.log(myArr);
 
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+// const newArr = myArr.join()  ////<-- join just combines the element and converts it to string , so datatype of final result would be string.
 // console.log(myArr);
 // console.log(newArr);
-// console.log(typeof newArr);
+// console.log(typeof newArr); //string
 
 
 
-// slice, splice
-console.log("A ", myArr); //A  [ 0, 1, 2, 3, 4, 5 ]
+// // slice, splice
+// console.log("A ", myArr); //A  [ 0, 1, 2, 3, 4, 5 ]
 
-const myn1 = myArr.slice(1, 3)
+// const myn1 = myArr.slice(1, 3)
 
-console.log(myn1); //[ 1, 2 ]
-console.log("B ", myArr); //B  [ 0, 1, 2, 3, 4, 5 ]
+// console.log(myn1); //[ 1, 2 ]
+// console.log("B ", myArr); //B  [ 0, 1, 2, 3, 4, 5 ]
 
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr); //C  [ 0, 4, 5 ]
-console.log(myn2); //[ 1, 2, 3 ]
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr); //C  [ 0, 4, 5 ]
+// console.log(myn2); //[ 1, 2, 3 ]
 
