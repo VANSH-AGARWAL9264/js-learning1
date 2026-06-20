ea786a86ffb6d22edb062ff4f502a659239bb5fd
VANSH-AGARWAL9264 <3pm2022vansh@gmail.com>
2026-06-20 18:07:23 +0000

    some new

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -7,20 +7,30 @@
 // chai()
 
 
-// // IIFE me sirf bracket ke andr likhte he function and uska execution:-
-(function chai() {
-    // //this is named IIFE bcs iska name he chai.
-    console.log(`DB Connected`);
-}) (); //<-- yaha pr semicolon lgana zaruri he kyuki hum IIFE ko invoke to kr de rahe he pr end krna zaruri he.
+// // // IIFE me sirf bracket ke andr likhte he function and uska execution:-
+// (function chai() {
+//     // //this is named IIFE bcs iska name he chai.
+//     console.log(`DB Connected`);
+// }) (); //<-- yaha pr semicolon lgana zaruri he kyuki hum IIFE ko invoke to kr de rahe he pr end krna zaruri he.
 
-( () => {
-    console.log(`DB Connected two`);
-}) ();
+// ( () => {
+//     console.log(`DB Connected two`);
+// }) ();
 
-( (name) => {
-    console.log(`DB Connected three ${name}`);
-}) ('hitesh');
+// ( (name) => {
+//     console.log(`DB Connected three ${name}`);
+// }) ('hitesh');
 
 // () <-1st bracket for function defintion & ()<-2nd bracket is for function execution.
 
 // //NOTE: why IIFE is used? <-- sirf ye immediately function execute krne ke liye use nahi hota he, but kai bar hume global scope ke pollution se problem hoti he to uss global scope ke jo variable ya pollution he usko htane ke liye ye use hota he.
+
+// // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// How js executes code + call stack:- watch chai or code video 25 of js.
+// // Js Execution Context:-
+
+// NOTE: js is a single threaded 
+
+// 1. Global Execution Context
+// 2. Functional Execution Context
+// 3. Eval Execution Context
